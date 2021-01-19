b23f37e0bb01b1d007ecf16c0a3a77762d80a56c
Suba1023 <74292943+Suba1023@users.noreply.github.com>
2021-01-19 11:53:54 +0530

    cmt

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,56 +27,44 @@ class Login extends Component {
     onSubmit=(e)=>{
         e.preventDefault();
         const {user_name,password}=this.state;
-        if(user_name !=='' && password !==''){
-            axios.post('http://www.json-generator.com/api/json/get/bUmzzXVWTC?indent=2',{user_name,password})
-                .then(function(response){
-                    console.log(response)
-                    console.log(this.state,"user_name,password")
-                    console.log("action=login")
-                }
-                )
-                .catch(err =>{
-                    console.log(err)
-                    console.log(this.state,"user_name,password")
-                    console.log("action=login")
-                })   
-     }
+        
     }
+    
 
     render() {
         const {user_name,password}=this.state;
         return (
             <div className="login" >
 
-                <Row>
+                <Row style={{top:"50%"}}>
                 <Col lg={6} sm={6}>
                     
-                <Image src={topimage} style={{width:"150px", marginTop:"100px"}} />
+                <Image src={topimage} style={{width:"15%", marginTop:"10%"}} />
                 <Row style={{marginTop:"40px"}}>
                     <h2 className="login-title">Crystal Clear Management- </h2>
-                    <h2 className="login-title">Leading Facilities Management In Asia</h2>
+                    <h2 className="login-title">Leading Facilities Management Service In Asia</h2>
                 </Row>
                 
                 
                 <div class="form-group row">
                     <div class="col-xs-2">
-                    <i className="fa fa-user-circle" style={{fontSize:"30px",position:"relative",top:"20%",right:"25%"}}></i>
+                    <i className="fa fa-user-circle" style={{fontSize:"220%",position:"relative",top:"20%",right:"25%"}}></i>
                         
                         <Form.Control 
                             className="login-input"
                             name="user_name" 
                             type="text" 
-                            placeholder="User Name" 
+                            placeholder="Username" 
                             onChange={this.onChange}
                          />
 
 
-                        <i class="fa fa-lock" aria-hidden="true"  style={{fontSize:"30px",position:"relative",top:"43%",right:"25%"}}></i>
+                        <i class="fa fa-lock" aria-hidden="true"  style={{fontSize:"220%",position:"relative",top:"43%",right:"25%"}}></i>
                         <Form.Control
                             className="login-input"
                             style={{marginTop:"50px"}}
                             name="password" 
-                            type="password" 
+                            type="text" 
                             placeholder="Password" 
                             onChange =  {this.onChange}  
                          />
@@ -85,18 +73,19 @@ class Login extends Component {
                 
                 
                 <br></br>
-                </Col>
-                </Row>
-                <Row>
-                    <Col lg={4} sm={4} style={{marginTop:"30px"}}>
-                        <h6 style={{fontWeight:"bold"}}>forget your password?</h6>
+                <Row style={{marginTop:"5%",marginLeft:"7%"}}>
+                    <Col lg={6} sm={4} style={{marginTop:"10px"}}>
+                        <h5 style={{fontWeight:"bold"}}>forget your password?</h5>
                     
                     </Col>
-                    <Col lg={4} sm={4}>
+                    <Col lg={6} sm={4}>
                        <button className="login-button" type="button"  onClick={this.onSubmit}>Login</button>
                     </Col>
 
                 </Row>
+                </Col>
+                </Row>
+                
                
             </div>
         )
